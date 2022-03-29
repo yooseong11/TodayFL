4274f7b1d33a6745d24cbf772f2d50420ec60cac
meteor lee <82145837+meteor-or@users.noreply.github.com>
2022-03-29 19:46:48 +0900

    [Feat + Refactoring] 내일 전장도 나타내기 위해서 오늘 전장 함수 변경
    - tailwind cdn으로 불러옴

--- js/main.js
+++ js/main.js
@@ -1,32 +1,17 @@
+const frontlineList = ["제압", "봉바", "쇄빙", "온살"]
 
-		let 오늘전장 = ""
-		const today = moment()
-		const getFrontline = () => {
-			const defalutDay = moment('2022-03-16')
-			
-			let data = today.diff(defalutDay,'days') % 4
-			switch (data) {
-				case 0:
-					오늘전장 = "제압"
-					break;
-			
-				case 1:
-					오늘전장 = "봉바"
-					break;
+const getFLturn = (firstDay, secondDay) => {
+	const FLtrun = Math.abs(secondDay.diff(firstDay,'days') % 4)
+	return FLtrun
+}
+const today = moment()
+const defalutDay = moment('2022-03-16')
+const todayTurn = getFLturn(today,defalutDay)
 
-				case 2:
-					오늘전장 = "쇄빙"
-					break;
+const getFLName = (int) => {
+	return frontlineList[int]
+}
 
-				case 3:
-					오늘전장 = "온살"
-					break;
-
-				default:
-					break;
-			}
-			return 오늘전장
-		}
-		getFrontline()
-		document.querySelector("#frontline").innerText = 오늘전장
-		document.querySelector("#date").innerText = today.format('YYYY년 MM월 D일')
+document.querySelector("#todayFL").textContent = getFLName(todayTurn)
+document.querySelector("#tomorrowFL").textContent = getFLName(todayTurn + 1)
+document.querySelector("#date").textContent = today.format('MM월 D일')
