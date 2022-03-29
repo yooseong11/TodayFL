7ad6957925352dafcc7e17c81bf7808dc4de1557
meteor lee <82145837+meteor-or@users.noreply.github.com>
2022-03-29 23:22:59 +0900

    [Fix] 오늘 전장이 온살이면 내일 전장이 안 뜨는 오류
    - 아... 이미 올렸는데 버그 발견

--- js/main.js
+++ js/main.js
@@ -1,17 +1,24 @@
 const frontlineList = ["제압", "봉바", "쇄빙", "온살"]
+const today = moment()
+const defalutDay = moment('2022-03-16')
+
 
 const getFLturn = (firstDay, secondDay) => {
 	const FLtrun = Math.abs(secondDay.diff(firstDay,'days') % 4)
 	return FLtrun
 }
-const today = moment()
-const defalutDay = moment('2022-03-16')
-const todayTurn = getFLturn(today,defalutDay)
-
+const getTomorrowTrun = (todayTurn) => {
+	let tomorrowTurn = todayTurn + 1
+	tomorrowTurn > frontlineList.length - 1 && (tomorrowTurn = 0)
+	
+	return tomorrowTurn
+}
 const getFLName = (int) => {
 	return frontlineList[int]
 }
 
+const todayTurn = getFLturn(today,defalutDay)
+const tomorrowTurn = getTomorrowTrun(todayTurn)
 document.querySelector("#todayFL").textContent = getFLName(todayTurn)
-document.querySelector("#tomorrowFL").textContent = getFLName(todayTurn + 1)
+document.querySelector("#tomorrowFL").textContent = getFLName(tomorrowTurn)
 document.querySelector("#date").textContent = today.format('MM월 D일')
