d6f5e492f5123c5b3dc394d29560ce2325a00cba
LeeYuseong <82145837+meteor-or@users.noreply.github.com>
2022-03-30 18:01:49 +0900

    [Refactoring] main.js

--- js/main.js
+++ js/main.js
@@ -1,23 +1,27 @@
 const frontlineList = ["제압", "봉바", "쇄빙", "온살"]
-const today = moment()
-const defalutDay = moment('2022-03-16')
 
 
 const getFLturn = (firstDay, secondDay) => {
-	const FLtrun = Math.abs(secondDay.diff(firstDay,'days') % 4)
-	return FLtrun
+	return FLtrun = Math.abs(secondDay.diff(firstDay,'days') % frontlineList.length)
 }
+
+const getTodayturn = () => {
+	const today = moment()
+	const defalutDay = moment('2022-03-16')
+	return getFLturn(today,defalutDay)
+}
+
 const getTomorrowTrun = (todayTurn) => {
 	let tomorrowTurn = todayTurn + 1
 	tomorrowTurn > frontlineList.length - 1 && (tomorrowTurn = 0)
-	
 	return tomorrowTurn
 }
+
 const getFLName = (int) => {
 	return frontlineList[int]
 }
 
-const todayTurn = getFLturn(today,defalutDay)
+const todayTurn = getTodayturn()
 const tomorrowTurn = getTomorrowTrun(todayTurn)
 document.querySelector("#todayFL").textContent = getFLName(todayTurn)
 document.querySelector("#tomorrowFL").textContent = getFLName(tomorrowTurn)
