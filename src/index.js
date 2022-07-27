ad8fbc42336b533c73749b122bd93fd9bbe143d3
LeeYuseong <82145837+meteor-or@users.noreply.github.com>
2022-07-27 09:58:22 +0900

    Feat: Webpack 설정

---
+++ src/index.js
@@ -0,0 +1,4 @@
+import './css/index.css';
+import './css/font.css';
+
+console.log('Im from source!');
---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, './dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: ['babel-loader'],
+      },
+    ],
+  },
+  plugins: [new HtmlWebpackPlugin({ template: './src/index.html' })],
+};
