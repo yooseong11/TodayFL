33db1b6ec6bd4e5dd90d081c4d6d1e3f34b2fd1d
유성 <82145837+yooseong11@users.noreply.github.com>
2023-11-29 21:14:54 +0900

    fix: 6.4 패치 대응

--- src/data.ts
+++ src/data.ts
@@ -15,7 +15,7 @@ export const elementID = {
   langMenu:"languages-menu"
 };
 
-export const defalutDate = '2022-03-16'; // 제압
+export const defalutDate = '2023-11-03'; // 제압
 
 interface LocalesType  {
   [key: string]: {
